eb74b60e13b65523ca3b3d1d69efac439aa966d4
Rob Wallace <rob@wallace.gen.nz>
2011-04-17 11:10:34 +1200

    Changes DB conversion script to remove any information not required for this application

--- repeaters/data/update_db.sh
+++ repeaters/data/update_db.sh
@@ -6,7 +6,7 @@ URL_FILE="spectrum-search-lite-database"
 DB_MDB="prism.mdb"
 DB_SQLITE="prism.sqlite"
 
-wget ${URL_PATH}/${URL_FILE}
+wget ${URL_PATH}/${URL_FILE} -O ${URL_FILE}
 unzip ${URL_FILE}
 
 mv ${DB_SQLITE} ${DB_SQLITE}.bak
@@ -23,3 +23,22 @@ for i in $(mdb-tables ${DB_MDB}); do echo $i; (
 
 rm ${URL_FILE}
 rm ${DB_MDB}
+
+sqlite3 ${DB_SQLITE} 'DROP TABLE IF EXISTS associatedlicences;'
+sqlite3 ${DB_SQLITE} 'DROP TABLE IF EXISTS emission;'
+sqlite3 ${DB_SQLITE} 'DROP TABLE IF EXISTS emissionlimit;'
+sqlite3 ${DB_SQLITE} 'DROP TABLE IF EXISTS issuingoffice;'
+sqlite3 ${DB_SQLITE} 'DROP TABLE IF EXISTS licenceconditions;'
+sqlite3 ${DB_SQLITE} 'DROP TABLE IF EXISTS licencetype;'
+sqlite3 ${DB_SQLITE} 'DROP TABLE IF EXISTS managementright;'
+sqlite3 ${DB_SQLITE} 'DROP TABLE IF EXISTS mapdistrict;'
+sqlite3 ${DB_SQLITE} 'DROP TABLE IF EXISTS radiationpattern;'
+sqlite3 ${DB_SQLITE} 'DROP TABLE IF EXISTS receiveconfiguration;'
+sqlite3 ${DB_SQLITE} 'DELETE FROM licence WHERE licencetype NOT LIKE "Amateur%";'
+sqlite3 ${DB_SQLITE} 'DELETE FROM clientname WHERE clientid NOT IN (SELECT DISTINCT clientid FROM licence);'
+sqlite3 ${DB_SQLITE} 'DELETE FROM spectrum WHERE licenceid NOT IN (SELECT DISTINCT licenceid FROM licence);'
+sqlite3 ${DB_SQLITE} 'DELETE FROM transmitconfiguration WHERE licenceid NOT IN (SELECT DISTINCT licenceid FROM licence);'
+sqlite3 ${DB_SQLITE} 'DELETE FROM spectrum WHERE licenceid NOT IN (SELECT DISTINCT licenceid FROM licence);'
+sqlite3 ${DB_SQLITE} 'DELETE FROM location WHERE locationid NOT IN (SELECT DISTINCT locationid FROM transmitconfiguration);'
+sqlite3 ${DB_SQLITE} 'DELETE FROM geographicreference WHERE locationid NOT IN (SELECT DISTINCT locationid FROM transmitconfiguration);'
+sqlite3 ${DB_SQLITE} 'VACUUM'
