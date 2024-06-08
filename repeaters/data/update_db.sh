47be3906850ae085c612cc25d32012ac4e4f5051
Rob Wallace <rob@wallace.gen.nz>
2024-06-08 21:37:08 +1200

    Tidy up data update script

--- repeaters/data/update_db.sh
+++ repeaters/data/update_db.sh
@@ -30,14 +30,14 @@ NEEDED_TABLES=(licence
 	           location
                geographicreference)
 
-UNNEEDED_TABLES=(associatedlicences
-                 emissionlimit
-                 issuingoffice
-                 licenceconditions
-                 licencetype
-                 managementright
-                 mapdistrict
-                 radiationpattern)
+#UNNEEDED_TABLES=(associatedlicences
+#                 emissionlimit
+#                 issuingoffice
+#                 licenceconditions
+#                 licencetype
+#                 managementright
+#                 mapdistrict
+#                 radiationpattern)
 
 # Get and unzip the source database file
 wget -q ${URL_PATH} -O ${URL_FILE}
@@ -50,14 +50,14 @@ unzip -qof ${URL_FILE}
 mv ${DB_SQLITE} ${DB_SQLITE}.bak
 
 # loop through the required tables creating the table and  
-for i in ${NEEDED_TABLES[@]}; do
-  mdb-schema -T $i ${DB_MDB}| perl -wpe 's%^DROP TABLE %DROP TABLE IF EXISTS %i;
+for i in "${NEEDED_TABLES[@]}"; do
+  mdb-schema -T "$i" ${DB_MDB}| perl -wpe 's%^DROP TABLE %DROP TABLE IF EXISTS %i;
     s%(Memo/Hyperlink|DateTime( \(Short\))?)%TEXT%i;
     s%(Boolean|Byte|Byte|Numeric|Replication ID|(\w+ )?Integer)%INTEGER%i;
     s%(BINARY|OLE|Unknown ([0-9a-fx]+)?)%BLOB%i;
     s%\s*\(\d+\)\s*(,?[ \t]*)$%${1}%;' | sqlite3 ${DB_SQLITE}
   (echo "BEGIN TRANSACTION;";
-  MDB_JET3_CHARSET=cp1256 mdb-export -R ";\n" -I mysql ${DB_MDB} $i;
+  MDB_JET3_CHARSET=cp1256 mdb-export -R ";\n" -I mysql ${DB_MDB} "$i";
   echo "END TRANSACTION;" ) | sed 's/ *"/\"/g' | sqlite3 ${DB_SQLITE};
 done
 
